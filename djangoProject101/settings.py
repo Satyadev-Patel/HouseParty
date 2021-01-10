600c6c63a8f675a58203a7887a661b108971f9c7
Satyadev-Patel <satyadev300302@gmail.com>
2021-01-10 21:15:49 +0530

    frontend updated

--- djangoProject101/settings.py
+++ djangoProject101/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'houseparty.apps.HousepartyConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
--- frontend/urls.py
+++ frontend/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import index
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('houseparty.urls')),
-    path('',include('frontend.urls'))
+    path('', index, name = 'index')
 ]
